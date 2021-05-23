229910f1d005d2bcd0970e89053a1266c80a18cd
Janu <janu.m.sung@gmail.com>
2021-05-23 10:47:02 -0700

    swapped the firebase SDK config variables and for some reason all the css from bulma is not showing up

--- src/utils/firebase.jsx
+++ src/utils/firebase.jsx
@@ -2,13 +2,13 @@ import firebase from "firebase";
 import "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_apiKey,
-  authDomain: process.env.REACT_APP_authDomain,
-  projectId: process.env.REACT_APP_projectId,
-  storageBucket: process.env.REACT_APP_storageBucket,
-  messagingSenderId: process.env.REACT_APP_messagingSenderId,
-  appId: process.env.REACT_APP_appId,
-  measurementId: process.env.REACT_APP_measurementId,
+  apiKey: "AIzaSyDcK-IRNfGYR37i_c8izXm2yekMegSqOSg",
+  authDomain: "dance-chives.firebaseapp.com",
+  projectId: "dance-chives",
+  storageBucket: "dance-chives.appspot.com",
+  messagingSenderId: "649724317145",
+  appId: "1:649724317145:web:52f24e40b99dc345c21dc8",
+  measurementId: "G-5HCQP4SBZL",
 };
 
 firebase.initializeApp(firebaseConfig);
