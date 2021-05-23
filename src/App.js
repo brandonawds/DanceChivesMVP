8d8013b34b7ef0d98cbab44f846b0aec9e42a9a6
Janu <janu.m.sung@gmail.com>
2021-05-23 10:21:00 -0700

    app and css changed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import "./App.css";
+// * Component Imports
+import RoutesTree from "./Components/RoutesTree";
+import Header from "./Components/SharedComponents/Header";
+import Footer from "./Components/SharedComponents/Footer";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <RoutesTree />
+      <Footer />
     </div>
   );
 }
