5a6f84d13b09feaee329a92a9bb42df1e2004906
Janu <janu.m.sung@gmail.com>
2021-05-29 09:53:05 -0700

    cleaning us css

--- src/Components/SharedComponents/NavBar.jsx
+++ src/Components/SharedComponents/NavBar.jsx
@@ -64,90 +64,91 @@ const NavBar = () => {
   return (
     <div className="navBar">
       <p>Logo</p>
-      <div className={toggleStyles ? "dropdown is-active" : "dropdown"}>
-        <div className="dropdown-trigger">
-          <button
-            className="button"
-            aria-haspopup="true"
-            aria-controls="dropdown-menu2"
-            onClick={() => toggleMenu("styles")}
-          >
-            <span>Styles</span>
-            <span className="icon is-small">
-              <i className="fas fa-angle-down" aria-hidden="true"></i>
-            </span>
-          </button>
-        </div>
-        <div className="dropdown-menu" id="dropdown-menu2" role="menu">
-          <div className="dropdown-content">
-            <li className="dropdown-item">
-              <Link to={"hip-hop"}>Hip-Hop</Link>
-            </li>
-            <li className="dropdown-item">
-              <Link to={"/breaking"} replace>
-                Breaking
-              </Link>
-            </li>
-            <li className="dropdown-item">
-              <Link to={"/popping"} replace>
-                Popping
-              </Link>
-            </li>
-            <li className="dropdown-item">
-              <Link to={"/house"} replace>
-                House
-              </Link>
-            </li>
-            <li className="dropdown-item">
-              <Link to={"/waacking"} replace>
-                Waacking
-              </Link>
-            </li>
-            <li className="dropdown-item">
-              <Link to={"/locking"} replace>
-                Locking
-              </Link>
-            </li>
-            <li className="dropdown-item">
-              <Link to={"/vogueing"} replace>
-                Vogue-ing
-              </Link>
-            </li>
+      <div className="links">
+        <div className={toggleStyles ? "dropdown is-active" : "dropdown"}>
+          <div className="dropdown-trigger">
+            <button
+              className="button"
+              aria-haspopup="true"
+              aria-controls="dropdown-menu2"
+              onClick={() => toggleMenu("styles")}
+            >
+              <span>Styles</span>
+              <span className="icon is-small">
+                <i className="fas fa-angle-down" aria-hidden="true"></i>
+              </span>
+            </button>
           </div>
-        </div>
-      </div>
-      <div className={toggleEvents ? "dropdown is-active" : "dropdown"}>
-        <div className="dropdown-trigger">
-          <button
-            className="button"
-            aria-haspopup="true"
-            aria-controls="dropdown-menu2"
-            onClick={() => toggleMenu("events")}
-          >
-            <span>Events</span>
-            <span className="icon is-small">
-              <i className="fas fa-angle-down" aria-hidden="true"></i>
-            </span>
-          </button>
-        </div>
-        <div className="dropdown-menu" id="dropdown-menu2" role="menu">
-          <div className="dropdown-content">
-            <div className="dropdown-item">
-              <Link to={"/find_events"}>Find Events</Link>
-            </div>
-            <div className="dropdown-item">
-              <Link to={"/recent_events"}>Recent Events</Link>
-            </div>
-            <div className="dropdown-item">
-              <Link to={"/submit_event_info"}>Submit Event Info</Link>
+          <div className="dropdown-menu" id="dropdown-menu2" role="menu">
+            <div className="dropdown-content">
+              <li className="dropdown-item">
+                <Link to={"hip-hop"}>Hip-Hop</Link>
+              </li>
+              <li className="dropdown-item">
+                <Link to={"/breaking"} replace>
+                  Breaking
+                </Link>
+              </li>
+              <li className="dropdown-item">
+                <Link to={"/popping"} replace>
+                  Popping
+                </Link>
+              </li>
+              <li className="dropdown-item">
+                <Link to={"/house"} replace>
+                  House
+                </Link>
+              </li>
+              <li className="dropdown-item">
+                <Link to={"/waacking"} replace>
+                  Waacking
+                </Link>
+              </li>
+              <li className="dropdown-item">
+                <Link to={"/locking"} replace>
+                  Locking
+                </Link>
+              </li>
+              <li className="dropdown-item">
+                <Link to={"/vogueing"} replace>
+                  Vogue-ing
+                </Link>
+              </li>
             </div>
           </div>
         </div>
+        <div className={toggleEvents ? "dropdown is-active" : "dropdown"}>
+          <div className="dropdown-trigger">
+            <button
+              className="button"
+              aria-haspopup="true"
+              aria-controls="dropdown-menu2"
+              onClick={() => toggleMenu("events")}
+            >
+              <span>Events</span>
+              <span className="icon is-small">
+                <i className="fas fa-angle-down" aria-hidden="true"></i>
+              </span>
+            </button>
+          </div>
+          <div className="dropdown-menu" id="dropdown-menu2" role="menu">
+            <div className="dropdown-content">
+              <div className="dropdown-item">
+                <Link to={"/find_events"}>Find Events</Link>
+              </div>
+              <div className="dropdown-item">
+                <Link to={"/recent_events"}>Recent Events</Link>
+              </div>
+              <div className="dropdown-item">
+                <Link to={"/submit_event_info"}>Submit Event Info</Link>
+              </div>
+            </div>
+          </div>
+        </div>
+        <Link to={"/"}>Home</Link>
+
+        <SearchBar />
       </div>
-      <Link to={"/"}>Home</Link>
-      {/* <Link to={"/styles"}>Styles</Link> */}
-      <Link to={"/events"}>Events</Link>
-      <SearchBar />
       {loggedInUser ? (
         <button onClick={signOut}>Logout </button>
       ) : (
